8286fd68955168b3078582147d09b152c9e25f86
Ben Jefferson <b9021400@my.shu.ac.uk>
2021-04-15 11:55:51 +0100

    updating files

--- CRUDFilmDbProject/wwwroot/js/site.js
+++ CRUDFilmDbProject/wwwroot/js/site.js
@@ -5,30 +5,6 @@
         $(this).toggleClass("animateBurger");
         $(".mainNav").slideToggle("800");
     });
-
-    /*
-    $(window).on("resize", function(ev) {
-        //console.info(window.innerWidth);
-        if (window.innerWidth > 800) {
-            $("nav ul").attr("style", "");
-        }
-    });
-
-    // Highlight function for navigation 
-    function navHighlight(elem, home, active) {
-        var url = location.href.split('/'),
-            loc = url[url.length - 1],
-            link = document.querySelectorAll(elem);
-        for (var i = 0; i < link.length; i++) {
-            var path = link[i].href.split('/'),
-                page = path[path.length - 1];
-            if (page == loc || page == home && loc == '') {
-                link[i].parentNode.className += ' ' + active;
-                document.body.className += ' ' + page.substr(0, page.lastIndexOf('.'));
-            }
-        }
-    }
-    navHighlight('.mainNav ul li a', 'index.html', 'current'); /* menu link selector, home page, highlight class */
 }
 )();
 
@@ -36,17 +12,14 @@
 // Password checking function, whitelists 
 $('input').on('keypress', function(event) {
     var newInput = new RegExp("^[a-zA-Z0-9 !@$.?,+]"); // white listed characters
-    /*
-    var r = [/0-9/, /a-z/, /A-Z/]; // Test if password contains one uppercase, lowercase and number
-    var goodPass = r.every(function (testPass) { return r.testPass(newInput); });
-    */
+
     var checkSymbl = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-    if (!newInput.test(checkSymbl)) { // & testPass(newInput = TRUE)
+    if (!newInput.test(checkSymbl))
+    { 
         console.info("invalid character input");
         event.preventDefault();
         return false;
     }
-
 });
 
 //open drop down rows to
@@ -69,72 +42,3 @@ function OpenRows2(clickedRow)//rows id passed in from database management.php
             elements.item(i).classList.toggle("hidden"); // IF row is clicked, hide classList
     }
 }
-
-
-
-// Checking password entered matches that stored in database
-var checkPasswordsMatch = function() {
-    var user;
-    var pass = document.getElementById('password');
-    var passConfirm = document.getElementById('confirm_password');
-    var key = document.getElementById('signUpKey');
-
-    if (pass.value == passConfirm.value) {
-        pass.style.borderColor = 'green';
-        passConfirm.style.borderColor = 'green';
-        document.getElementById("signUpSubmit").disabled = false;
-    }
-    else if (user.value == "" && pass.value == "" && user.value == "" && user.value == "") {
-        document.getElementById("signUpSubmit").disabled = true;
-
-    }
-    else {
-        document.getElementById('password').style.borderColor = 'red';
-        document.getElementById('confirm_password').style.borderColor = 'red';
-        document.getElementById("signUpSubmit").disabled = true;
-
-    }
-};
-
-//Functions to check if passwords match
-
-var checkPasswordsMatchReset = function() {
-
-    var pass = document.getElementById('password');
-    var passConfirm = document.getElementById('confirm_password');
-
-    //If both passwords match
-
-    if (pass.value == passConfirm.value) {
-        pass.style.borderColor = 'green';
-        passConfirm.style.borderColor = 'green';
-        document.getElementById("signUpSubmit").disabled = false;
-    }
-
-    //if they do not match
-
-    else if (pass.value == "" || passConfirm.value == "") {
-        document.getElementById("signUpSubmit").disabled = true;
-
-    }
-    else {
-        document.getElementById('password').style.borderColor = 'red';
-        document.getElementById('confirm_password').style.borderColor = 'red';
-        document.getElementById("signUpSubmit").disabled = true;
-
-    }
-};
-
-//function to copy text to clipboard
-
-function copyToClipboard(text) {
-    console.info("copy function");
-    console.info(text);
-
-    var dummy = document.createElement("textarea");
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
-};
